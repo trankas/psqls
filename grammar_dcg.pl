0966d282d170fc55d14ae738697b37bade3f3cc2
João Estêvão <trankas@gmail.com>
2010-12-15 22:18:19 +0000

    example DCG grammar

---
+++ grammar_dcg.pl
@@ -0,0 +1,14 @@
+   s --> np, vp.
+    np --> det, n.
+    vp --> tv, np.
+    vp --> v.
+    det --> [the].
+    det --> [a].
+    det --> [every].
+    n --> [man].
+    n --> [woman].
+    n --> [park].
+    tv --> [loves].
+    tv --> [likes].
+    v --> [walks].
+
