532008ae7cb7d6a7e5aea08714ae941c62d55e51
João Estêvão <trankas@gmail.com>
2012-10-26 18:52:32 +0200

    Lots of bugfixes

--- sql_grammar.pl
+++ sql_grammar.pl
@@ -1,49 +1,54 @@
 %TODO: support 'nulls last'
 
+select(select_node(S)) --> nonreq_subquery(S).
 select(select_node(S)) --> subquery(S).
 
-subquery(subquery_node(SQ)) --> { write('subquery1\n')},query_block(SQ).
-subquery(subquery_node(SQ, OB)) --> { write('subquery2\n')},query_block(SQ), top_order_by_clause(OB).
-subquery(subquery_node(SQL, 'UNION', SQR)) --> { write('subquery3\n')},subquery(SQL), ['UNION'], subquery(SQR).
-subquery(subquery_node(SQL, 'UNION', SQR, OB)) --> subquery(SQL), ['UNION'], subquery(SQR), top_order_by_clause(OB).
-subquery(subquery_node(SQL, 'UNION', 'ALL', SQR)) --> { write('subquery4\n')},subquery(SQL), ['UNION'], ['ALL'], subquery(SQR).
-subquery(subquery_node(SQL, 'UNION', 'ALL', SQR, OB)) --> subquery(SQL), ['UNION'], ['ALL'], subquery(SQR), top_order_by_clause(OB).
-subquery(subquery_node(SQL, 'INTERSECT', SQR)) --> { write('subquery5\n')},subquery(SQL), ['INTERSECT'], subquery(SQR).
-subquery(subquery_node(SQL, 'INTERSECT', SQR, OB)) --> subquery(SQL), ['INTERSECT'], subquery(SQR), top_order_by_clause(OB).
-subquery(subquery_node(SQL, 'MINUS', SQR)) --> { write('subquery6\n')},subquery(SQL), ['MINUS'], subquery(SQR).
-subquery(subquery_node(SQL, 'MINUS', SQR, OB)) --> subquery(SQL), ['MINUS'], subquery(SQR), top_order_by_clause(OB).
-subquery(subquery_node('(', SQ, ')')) --> ['('], { write('subquery7\n')},subquery(SQ), [')'].
-subquery(subquery_node('(', SQ, ')', OB)) --> ['('], subquery(SQ), [')'], top_order_by_clause(OB).
+subquery(subquery_node(SQL, 'UNION', SQR)) --> nonreq_subquery(SQL), ['UNION'], subquery(SQR).
+subquery(subquery_node(SQL, 'UNION', SQR, OB)) --> nonreq_subquery(SQL), ['UNION'], subquery(SQR), top_order_by_clause(OB).
+subquery(subquery_node(SQL, 'UNION ALL', SQR)) --> nonreq_subquery(SQL), ['UNION ALL'], subquery(SQR).
+subquery(subquery_node(SQL, 'UNION ALL', SQR, OB)) --> nonreq_subquery(SQL), ['UNION ALL'], subquery(SQR), top_order_by_clause(OB).
+subquery(subquery_node(SQL, 'INTERSECT', SQR)) --> nonreq_subquery(SQL), ['INTERSECT'], subquery(SQR).
+subquery(subquery_node(SQL, 'INTERSECT', SQR, OB)) --> nonreq_subquery(SQL), ['INTERSECT'], subquery(SQR), top_order_by_clause(OB).
+subquery(subquery_node(SQL, 'MINUS', SQR)) --> nonreq_subquery(SQL), ['MINUS'], subquery(SQR).
+subquery(subquery_node(SQL, 'MINUS', SQR, OB)) --> nonreq_subquery(SQL), ['MINUS'], subquery(SQR), top_order_by_clause(OB).
+subquery(subquery_node('(', SQ, ')')) --> ['('], nonreq_subquery(SQ), [')'].
+subquery(subquery_node('(', SQ, ')', OB)) --> ['('], nonreq_subquery(SQ), [')'], top_order_by_clause(OB).
 
-query_block(query_block_node(SP,SLP,FP,TRP)) --> {write('query_block1\n')}, select_block(SP), select_list(SLP), from(FP), table_reference(TRP).
-query_block(query_block_node(SP,SLP,FP,TRP,WC)) --> {write('query_block2\n')},select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC).
-%query_block(query_block_node(SP,SLP,FP,TRP,OBC)) --> {write('query_block3\n)'},select_block(SP), select_list(SLP), from(FP), table_reference(TRP), top_order_by_clause(OBC).
-query_block(query_block_node(SP,SLP,FP,TRP,GC)) --> {write('query_block4\n')},select_block(SP), select_list(SLP), from(FP), table_reference(TRP), group_by_clause(GC).
-%query_block(query_block_node(SP,SLP,FP,TRP,GC,OBC)) --> {write('query_block5\n')},select_block(SP), select_list(SLP), from(FP), table_reference(TRP), group_by_clause(GC), top_order_by_clause(OBC).
-%query_block(query_block_node(SP,SLP,FP,TRP,WC,OBC)) --> {write('query_block6\n')},select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC), top_order_by_clause(OBC).
-query_block(query_block_node(SP,SLP,FP,TRP,WC,GC)) --> {write('query_block7\n')},select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC), group_by_clause(GC).
-%query_block(query_block_node(SP,SLP,FP,TRP,WC,GC,OBC)) --> {write('query_block8\n')},select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC), group_by_clause(GC), top_order_by_clause(OBC).
+nonreq_subquery(subquery_node(SQ)) --> query_block(SQ).
+nonreq_subquery(subquery_node(SQ, OB)) --> query_block(SQ), top_order_by_clause(OB).
+
+query_block(query_block_node(SP,SLP,FP,TRP)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP).
+query_block(query_block_node(SP,SLP,FP,TRP,WC)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC).
+%DUP? query_block(query_block_node(SP,SLP,FP,TRP,OBC)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP), top_order_by_clause(OBC).
+query_block(query_block_node(SP,SLP,FP,TRP,GC)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP), group_by_clause(GC).
+%DUP? query_block(query_block_node(SP,SLP,FP,TRP,GC,OBC)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP), group_by_clause(GC), top_order_by_clause(OBC).
+%DUP? query_block(query_block_node(SP,SLP,FP,TRP,WC,OBC)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC), top_order_by_clause(OBC).
+query_block(query_block_node(SP,SLP,FP,TRP,WC,GC)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC), group_by_clause(GC).
+%DUP? query_block(query_block_node(SP,SLP,FP,TRP,WC,GC,OBC)) --> select_block(SP), select_list(SLP), from(FP), table_reference(TRP), where_clause(WC), group_by_clause(GC), top_order_by_clause(OBC).
 
 select_block(select_kw('select')) --> ['select'].
 
-select_list(select_list_node('*')) --> ['*'].
-select_list(select_list_node(SELECT_TERM)) --> [SELECT_TERM].
-select_list(select_list_node(SELECT_TERM, ',', REST)) --> [SELECT_TERM], [','], select_list(REST).
+select_list(select_list_node(S)) --> select_item(S).
+%select_list(select_list_node(SELECT_TERM, ',', REST)) --> [SELECT_TERM], [','], select_list(REST).
+select_list(select_list_node(SELECT_ITEM, ',', REST)) --> select_item(SELECT_ITEM), [','], select_list(REST).
+
+select_item(select_item_node('*')) --> ['*'].
+select_item(select_item_node(SELECT_TERM, 'as', ALIAS)) --> [SELECT_TERM], ['as'], [ALIAS].
+select_item(select_item_node(SELECT_TERM)) --> [SELECT_TERM].
 
 from(from_kw('from')) --> ['from'].
 
-table_reference(table_reference_node(TA)) --> [TA].
-table_reference(table_reference_node('(', SQ, ')')) --> ['('], subquery(SQ), [')'].
-table_reference(table_reference_node(JC)) --> join_clause(JC).
+table_reference(table_reference_node('(', SQ, ')')) --> ['('], select(SQ), [')'].
 table_reference(table_reference_node('(', JC, ')')) --> ['('], join_clause(JC), [')'].
+table_reference(table_reference_node(TA)) --> [TA].
+table_reference(table_reference_node(JC)) --> join_clause(JC).
 
-
-join_clause(join_clause_node(TA, 'inner join', TB, 'on', CONDITION)) --> [TA], ['inner join'], {!}, [TB], ['on'], condition(CONDITION).
-join_clause(join_clause_node(TA, 'outer join', TB, 'on', CONDITION)) --> [TA], ['outer join'], {!}, [TB], ['on'], condition(CONDITION).
-join_clause(join_clause_node(TA, 'left outer join', TB, 'on', CONDITION)) --> [TA], ['left outer join'], {!}, [TB], ['on'], condition(CONDITION).
-join_clause(join_clause_node(TA, 'right outer join', TB, 'on', CONDITION)) --> [TA], ['right outer join'], {!}, [TB], ['on'], condition(CONDITION).
-join_clause(join_clause_node(TA, 'left join', TB, 'on', CONDITION)) --> [TA], ['left join'], {!}, [TB], ['on'], condition(CONDITION).
-join_clause(join_clause_node(TA, 'right join', TB, 'on', CONDITION)) --> [TA], ['right join'], {!}, [TB], ['on'], condition(CONDITION).
+join_clause(join_clause_node(TA, 'inner join', TB, 'on', CONDITION)) --> [TA], ['inner join'], [TB], ['on'], condition(CONDITION).
+join_clause(join_clause_node(TA, 'outer join', TB, 'on', CONDITION)) --> [TA], ['outer join'], [TB], ['on'], condition(CONDITION).
+join_clause(join_clause_node(TA, 'left outer join', TB, 'on', CONDITION)) --> [TA], ['left outer join'], [TB], ['on'], condition(CONDITION).
+join_clause(join_clause_node(TA, 'right outer join', TB, 'on', CONDITION)) --> [TA], ['right outer join'], [TB], ['on'], condition(CONDITION).
+join_clause(join_clause_node(TA, 'left join', TB, 'on', CONDITION)) --> [TA], ['left join'], [TB], ['on'], condition(CONDITION).
+join_clause(join_clause_node(TA, 'right join', TB, 'on', CONDITION)) --> [TA], ['right join'], [TB], ['on'], condition(CONDITION).
 join_clause(join_clause_node(TA, 'join', TB, 'on', CONDITION)) --> [TA], ['join'], [TB], ['on'], condition(CONDITION).
 
 where_clause(where_clause_node('where', CONDITION)) --> ['where'], condition(CONDITION).
@@ -56,16 +61,16 @@ having_clause(having_clause_node(CONDITION)) --> condition(CONDITION).
 top_order_by_clause(top_order_by_clause_node('order by', REST)) --> ['order by'], order_by_clause(REST).
 
 order_by_clause(order_by_clause_node(EXPR, 'asc')) --> expr(EXPR).
-order_by_clause(order_by_clause_node(EXPR, 'asc', ',', REST)) --> expr(EXPR), [','], {!}, order_by_clause(REST).
+order_by_clause(order_by_clause_node(EXPR, 'asc', ',', REST)) --> expr(EXPR), [','], order_by_clause(REST).
 
 order_by_clause(order_by_clause_node(EXPR, 'asc')) --> expr(EXPR), ['asc'].
-order_by_clause(order_by_clause_node(EXPR, 'asc', ',', REST)) --> expr(EXPR), ['asc'], {!}, [','], order_by_clause(REST).
+order_by_clause(order_by_clause_node(EXPR, 'asc', ',', REST)) --> expr(EXPR), ['asc'], [','], order_by_clause(REST).
 
 order_by_clause(order_by_clause_node(EXPR, 'desc')) --> expr(EXPR), ['desc'].
-order_by_clause(order_by_clause_node(EXPR, 'desc', ',', REST)) --> expr(EXPR), ['desc'], {!}, [','], order_by_clause(REST).
+order_by_clause(order_by_clause_node(EXPR, 'desc', ',', REST)) --> expr(EXPR), ['desc'], [','], order_by_clause(REST).
 
 condition(condition_node(L, OP, R)) --> [L], operator(OP), [R].
-condition(condition_node(L, OP, R, LOGOP, COND_REST)) --> [L], operator(OP), [R], {!}, logical_operator(LOGOP), condition(COND_REST).
+condition(condition_node(L, OP, R, LOGOP, COND_REST)) --> [L], operator(OP), [R], logical_operator(LOGOP), condition(COND_REST).
 
 operator(op('=')) --> ['='].
 operator(op('>=')) --> ['>='].
@@ -94,9 +99,12 @@ expr(expr(E)) --> [E].
 
 %subquery(Tree, ['select', '*', 'from', '(', 'select', '*', 'from', 'a', 'group by', 'col1', 'order by', 'col1', 'desc', ')'], []).
 
+% LOOPS
 %select(Tree, ['select', '*', 'from', '(', 'select', '*', 'from', 'a', 'group by', 'col1', 'order by', 'col1', 'desc', ')'], []).
+% END LOOPS
 %select(Tree, ['select', '*', 'from', 'a', 'group by', 'col1', 'order by', 'col1', ',', 'col2'], []).
 %select(Tree, ['select', '*', 'from', 'a', 'group by', 'col1', 'order by', 'col1', ',', 'col2', 'asc'], []).
 %select(Tree, ['select', '*', 'from', 'a', 'group by', 'col1', 'order by', 'col1', 'desc', ',', 'col2', 'asc'], []).
 %select(Tree, ['select', '*', 'from', 'a', 'order by', 'col1', ',', 'col2', 'asc'], []).
 %top_order_by_clause(Tree, ['order by', 'col1', ',', 'col2', 'asc'], []).
+%select(Tree, ['select', '*', 'from', '(', 'select', '*', 'from', 'a', ')'], []).
